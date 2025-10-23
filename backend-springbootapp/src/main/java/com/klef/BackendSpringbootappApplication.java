eb7494f02bee4838d90b8ab3236cec6386aef6b9
klu2300031264 <2300031264@kluniversity.in>
2025-10-24 01:04:14 +0530

    ok ok

--- backend-springbootapp/src/main/java/com/klef/BackendSpringbootappApplication.java
+++ backend-springbootapp/src/main/java/com/klef/BackendSpringbootappApplication.java
@@ -1,10 +1,11 @@
 package com.klef;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendSpringbootappApplication {
+public class BackendSpringbootappApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendSpringbootappApplication.class, args);
